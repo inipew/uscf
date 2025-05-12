20438e190a79cc7527335bfa3290fadda49bdc84
HynoR <20227709+HynoR@users.noreply.github.com>
2025-05-12 10:58:01 +0800

    优化SOCKS5服务器认证和健康检查脚本，支持用户名和密码的URL解码与编码

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,23 +1,38 @@
 #!/bin/sh
 
-# 获取 SOCKS 端口
-PORT=$(jq -r '.socks.port' /app/etc/config.json)
+# 读取 SOCKS 配置
+CONFIG_PATH="/app/etc/config.json"
+PORT=$(jq -r '.socks.port' $CONFIG_PATH)
+USERNAME=$(jq -r '.socks.username' $CONFIG_PATH)
+PASSWORD=$(jq -r '.socks.password' $CONFIG_PATH)
+
+# 设置默认端口
 if [ -z "$PORT" ] || [ "$PORT" = "null" ]; then
     PORT="1080"  # 默认端口
 fi
 
 # 使用 curl 通过 SOCKS 代理检查连接
 check_url() {
-    curl --silent --connect-timeout 5 --max-time 10 -x "socks5h://127.0.0.1:$PORT" "$1" -o /dev/null -w "%{http_code}"
+    if [ -n "$USERNAME" ] && [ "$USERNAME" != "null" ] && [ -n "$PASSWORD" ] && [ "$PASSWORD" != "null" ]; then
+        curl --silent --connect-timeout 5 --max-time 10 --socks5 "127.0.0.1:$PORT" --proxy-user "$USERNAME:$PASSWORD" "$1" -o /dev/null -w "%{http_code}"
+    else
+        curl --silent --connect-timeout 5 --max-time 10 --socks5 "127.0.0.1:$PORT" "$1" -o /dev/null -w "%{http_code}"
+    fi
 }
 
-# 检查两个 URL
 status_gstatic=$(check_url "http://connectivitycheck.gstatic.com/generate_204")
-status_cloudflare=$(check_url "http://cp.cloudflare.com/")
 
-# 如果任一检查返回 204，则认为服务健康
-if [ "$status_gstatic" = "204" ] || [ "$status_cloudflare" = "204" ]; then
+
+if [ "$status_gstatic" = "204" ]; then
+    echo "[Health Check] OK(Google)"
+    exit 0
+fi
+
+status_cloudflare=$(check_url "http://cp.cloudflare.com/")
+if [ "$status_cloudflare" = "204" ]; then
+    echo "[Health Check] OK(Cloudflare)"
     exit 0
 else
+    echo "[Health Check] Failed!!!"
     exit 1
 fi
